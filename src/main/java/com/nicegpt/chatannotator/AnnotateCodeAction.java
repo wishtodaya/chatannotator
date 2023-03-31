649ee45b09e90b71c2539d41aff4cee81e1c5c18
zheng <zhengjunqiang@airlonghao.com>
2023-03-31 16:19:04 +0800

    在代码中，使用了 document.setText() 方法来更新文档内容，但是这个方法没有进行线程同步控制，因此可能会在多个线程同时访问时出现并发访问的问题。
    为了优化这个问题，可以使用 com.intellij.openapi.application.Application.runWriteAction() 方法将修改文档内容的代码放入一个写操作块中。这个方法会确保在多线程的情况下正确修改文档内容，避免出现并发访问的问题。

--- src/main/java/com/nicegpt/chatannotator/AnnotateCodeAction.java
+++ src/main/java/com/nicegpt/chatannotator/AnnotateCodeAction.java
@@ -1,7 +1,10 @@
+// 该类是AnAction的子类，用于处理用户在IDEA中点击插件按钮时的事件
 package com.nicegpt.chatannotator;
+
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileTypes.FileType;
@@ -9,26 +12,33 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 
 public class AnnotateCodeAction extends AnAction {
+    // actionPerformed方法重载了AnAction中的方法，在用户点击插件按钮时会自动执行该方法
     @Override
     public void actionPerformed(AnActionEvent e) {
+        // 获取当前的工程和编辑器对象
         Project project = e.getProject();
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
 
+        // 如果当前没有打开任何文件，弹出错误提示框并返回
         if (editor == null) {
             Messages.showMessageDialog(project, "Please open a file before running the plugin.", "Error", Messages.getErrorIcon());
             return;
         }
 
-        FileType fileType = e.getData(PlatformDataKeys.VIRTUAL_FILE).getFileType();
+        // 获取文件类型和代码内容
+        FileType fileType = e.getData(PlatformDataKeys.VIRTUAL_FILE).getFileType(); 
         String code = editor.getDocument().getText();
 
-        // Call the ChatGPT API here, and add the annotations to the code.
-        // Here's a simple example of how to add a comment at the beginning of the file:
+        // 调用ChatGPT API接口添加代码注释
+        // 这里是一个简单的示例，在文件开头添加了一行注释
         code = "为这段代码添加中文注释,不对代码进行改变,只允许添加注释,并且注释格式以chatGPT开头:" + code;
         try {
-            String  annotatedCode= ChatGPTApi.requestChatGpt(code);
-            Document document = editor.getDocument();
-            document.setText(annotatedCode);
+            String annotatedCode = ChatGPTApi.requestChatGpt(code);
+            // 将注释后的代码更新到编辑器中
+            ApplicationManager.getApplication().runWriteAction(() -> {
+                Document document = editor.getDocument();
+                document.setText(annotatedCode);
+            });
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
